ff14cb2a35ca78810900aa154ac81f9c261f9b6e
Vitalii <vitalynb8@gmail.com>
2026-04-19 01:16:39 +0300

    Update build script to clean build artifacts and use main.py as entry point
    Specific changes include:
    - Added commands to remove `build` and `dist` directories before building.
    - Added `--clean` and `--icon` flags to the PyInstaller command.
    - Changed the entry point script from `gui.py` to `main.py`.

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-pyinstaller --onefile --windowed --add-data "locale;locale" --name U-Prox-USB gui.py
+rd /s /q build
+rd /s /q dist
+pyinstaller --clean --onefile --windowed --add-data "locale;locale" --icon=icon.ico --name U-Prox-USB main.py
