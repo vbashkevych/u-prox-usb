a5fba032ffb6baa7769c53c612179b68c55b2b26
Vitalii <vitalynb8@gmail.com>
2026-05-09 23:30:52 +0300

    chore: setup new directory structure and move assets

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 rd /s /q build
 rd /s /q dist
-pyinstaller --clean --onefile --windowed --add-data "locale;locale" --icon=icon.ico --name U-Prox-USB main.py
+pyinstaller --clean --onefile --windowed --add-data "locale;locale" --icon=assets/icon.ico --name U-Prox-USB src/main.py
